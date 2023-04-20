f5167c6eb374ae24963e997ca573db9ba47497af
jswoo0615 <dnwnstkd122@gmail.com>
2023-04-20 21:51:37 +0900

    first-commit

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "First Posting",
+        "excerpt":"우준상  공사중  ","categories": [],
+        "tags": [],
+        "url": "/first-posting/",
+        "teaser": null
+      }]
