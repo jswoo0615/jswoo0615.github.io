71ff8cbc02a636f0e4605df10df935ce241f4dc9
jswoo0615 <dnwnstkd122@gmail.com>
2023-04-20 22:55:30 +0900

    404 등 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "첫 번째 블로그 글 입니다",
-        "excerpt":"우준상  공사중   #include &lt;iostream&gt;  int main() {   std::cout &lt;&lt; \"Hello\" &lt;&lt; std::endl; }  ","categories": ["coding"],
-        "tags": ["C++","Carla","blog"],
+        "excerpt":"우준상  이미지 목차  이미지 목차 1   이미지 목차 2   이미지 목차 3  공사중   #include &lt;iostream&gt;  int main() {   std::cout &lt;&lt; \"Hello\" &lt;&lt; std::endl; }   안뇽  ","categories": ["coding"],
+        "tags": ["C++","연습","blog"],
         "url": "/coding/first-posting/",
         "teaser": null
       }]
