f86ff491c06151190167718701b802b30ecee90b
jswoo0615 <dnwnstkd122@gmail.com>
2023-04-20 22:35:10 +0900

    내용 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "First Posting",
-        "excerpt":"우준상  공사중  ","categories": [],
-        "tags": [],
-        "url": "/first-posting/",
+        "title": "첫 번째 블로그 글 입니다",
+        "excerpt":"우준상  공사중   #include &lt;iostream&gt;  int main() {   std::cout &lt;&lt; \"Hello\" &lt;&lt; std::endl; }  ","categories": ["coding"],
+        "tags": ["C++","Carla","blog"],
+        "url": "/coding/first-posting/",
         "teaser": null
       }]
