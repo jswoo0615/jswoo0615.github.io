e34f36b12b546282f4e3428f88b465565786ce54
jswoo0615 <dnwnstkd122@gmail.com>
2023-04-21 22:08:43 +0900

    업데이트

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,13 +1,31 @@
 var store = [{
         "title": "첫 번째 블로그 글 입니다",
-        "excerpt":"우준상  이미지 목차  이미지 목차 1   이미지 목차 2   이미지 목차 3  공사중   #include &lt;iostream&gt;  int main() {   std::cout &lt;&lt; \"Hello\" &lt;&lt; std::endl; }   안뇽  ","categories": ["coding"],
-        "tags": ["C++","연습","blog"],
-        "url": "/coding/first-posting/",
+        "excerpt":"우준상  이미지 목차  이미지 목차 1   이미지 목차 2   이미지 목차 3  공사중   #include &lt;iostream&gt;  int main() {   std::cout &lt;&lt; \"Hello\" &lt;&lt; std::endl; }   안뇽  ","categories": [],
+        "tags": [],
+        "url": "/first-posting/",
+        "teaser": null
+      },{
+        "title": "Autoware_AI",
+        "excerpt":" ","categories": ["AD_Stack"],
+        "tags": ["Autoware.AI","AD_Stack"],
+        "url": "/ad_stack/Autoware-AI/",
+        "teaser": null
+      },{
+        "title": "Autoware_Universe",
+        "excerpt":" ","categories": ["AD_Stack"],
+        "tags": ["Autoware.Universe","AD_Stack"],
+        "url": "/ad_stack/Autoware-Universe/",
+        "teaser": null
+      },{
+        "title": "Carla Simulator 설치",
+        "excerpt":"1. 패키지 설치 - 의존성 패키지 설치 Linux build - CARLA Simulator 의 Part One : Prerequisite의 내용 설치 진행했다. sudo apt-get update &amp;&amp; sudo apt-get install wget software-properties-common &amp;&amp; sudo add-apt-repository ppa:ubuntu-toolchain-r/test &amp;&amp; wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add - &amp;&amp; sudo apt-add-repository \"deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main\" &amp;&amp; sudo...","categories": ["Carla_Simulator"],
+        "tags": ["Carla","자율주행 시뮬레이터","설치"],
+        "url": "/carla_simulator/Carla-simulator/",
         "teaser": null
       },{
         "title": "두 번째 블로그 글 입니다",
-        "excerpt":"우준상  이미지 목차  이미지 목차 1   이미지 목차 2   이미지 목차 3  공사중   #include &lt;iostream&gt;  int main() {   std::cout &lt;&lt; \"Hello\" &lt;&lt; std::endl; }   안뇽  ","categories": ["second"],
-        "tags": ["C++","연습","blog"],
-        "url": "/second/second-posting/",
+        "excerpt":"우준상  이미지 목차  이미지 목차 1   이미지 목차 2   이미지 목차 3  공사중   #include &lt;iostream&gt;  int main() {   std::cout &lt;&lt; \"Hello\" &lt;&lt; std::endl; }   안뇽  ","categories": [],
+        "tags": [],
+        "url": "/second-posting/",
         "teaser": null
       }]
