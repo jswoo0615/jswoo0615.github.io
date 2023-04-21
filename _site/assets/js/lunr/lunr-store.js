807fe872e66c6c6a2154b5711a9899c4056af20f
jswoo0615 <dnwnstkd122@gmail.com>
2023-04-21 13:00:44 +0900

    _site 빌드

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["C++","연습","blog"],
         "url": "/coding/first-posting/",
         "teaser": null
+      },{
+        "title": "두 번째 블로그 글 입니다",
+        "excerpt":"우준상  이미지 목차  이미지 목차 1   이미지 목차 2   이미지 목차 3  공사중   #include &lt;iostream&gt;  int main() {   std::cout &lt;&lt; \"Hello\" &lt;&lt; std::endl; }   안뇽  ","categories": ["second"],
+        "tags": ["C++","연습","blog"],
+        "url": "/second/second-posting/",
+        "teaser": null
       }]
